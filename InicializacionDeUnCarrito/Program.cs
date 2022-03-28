59d1e30fe17c93485e63edee2550973c4784d256
yarroyom <yarroyom@miumg.edu.gt>
2022-03-27 23:04:20 -0500

    first commit

---
+++ InicializacionDeUnCarrito/Program.cs
@@ -0,0 +1,80 @@
+﻿using InicializacionDeUnCarrito.clases;
+
+Carro miCarrito = new Carro("Toyota", 2022, "azul");
+Carro EstefaniCarrito = new Carro("Ferrari", 2022, "Rojo");
+Carro Yesicacarrito = new Carro("Prado", 2015, "Blanco", 250);
+
+if (miCarrito.Modelo > EstefaniCarrito.Modelo)
+{
+    Console.WriteLine("El carro del profe es mas reciente");
+}
+else
+{
+    Console.WriteLine("El carro de Estefani es mas reciente");
+}
+
+Console.WriteLine($"teacher car is {miCarrito.Marca}");
+Console.WriteLine($"Estafani's car is {EstefaniCarrito.Marca}");
+Console.WriteLine($"Estafani's model car is {EstefaniCarrito.Modelo}");
+
+EstefaniCarrito.EncendidoMotor();
+Console.WriteLine(EstefaniCarrito.GetVelocidadActual());
+//EstefaniCarrito.EncendidoMotor();
+
+
+for (int i = 0; i < 5; i++)
+{
+    EstefaniCarrito.acelerar();
+
+}
+
+
+
+
+for (int i = 0; i < 3; i++)
+{
+
+    EstefaniCarrito.desacelerar();
+
+}
+
+for (int i = 0; i < 5; i++)
+{
+
+    EstefaniCarrito.frenar();
+
+}
+
+EstefaniCarrito.apagarmotor();
+EstefaniCarrito.apagarmotor();
+EstefaniCarrito.EncendidoMotor();
+EstefaniCarrito.Bocinar();
+
+Console.WriteLine("\n");
+Console.WriteLine("Carro de Yesica");
+Console.WriteLine($"Marca: {Yesicacarrito.Marca}");
+Console.WriteLine($"Modelo: {Yesicacarrito.Modelo}");
+Console.WriteLine($"Color: {Yesicacarrito.Color}");
+Console.WriteLine($"Velocidad Maxima : {Yesicacarrito.VMaxima} KM/H");
+
+Yesicacarrito.EncendidoMotor();
+Yesicacarrito.EncendidoMotor();
+Console.WriteLine(Yesicacarrito.GetVelocidadActual());
+
+for (int i = 0; i < 15; i++)
+{
+    Yesicacarrito.acelerar();
+}
+for (int i = 0; i < 10; i++)
+{
+    Yesicacarrito.desacelerar();
+}
+for (int i = 0; i < 10; i++)
+{
+    Yesicacarrito.frenar();
+}
+Yesicacarrito.apagarmotor();
+Yesicacarrito.apagarmotor();
+Yesicacarrito.EncendidoMotor();
+Yesicacarrito.Bocinar();
+
---
+++ InicializacionDeUnCarrito/class/carro.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicializacionDeUnCarrito.clases
+{
+   
+        internal class Carro
+        {
+            public string Marca { get; }
+            public int Modelo { get; }
+            public String Color { get; set; }
+            public int VMaxima { get; }
+
+        private int Encendido = 0;
+            private int velocidad_actual = 0;
+            private const int MAXVELOCIDAD = 150;
+
+
+        public Carro(string _marca, int _modelo, string _color, int _vmaxima)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Color = _color;
+            VMaxima= _vmaxima; 
+            Encendido = 0;
+            this.velocidad_actual = 0;
+        }
+
+        public Carro(string v1, int v2, string v3)
+        {
+        }
+
+        public string GetVelocidadActual()
+        {
+            return $"vamos a {velocidad_actual} KM/H";
+        }
+
+        public void EncendidoMotor()
+        {
+            if (Encendido == 0)
+            {
+                Console.WriteLine("Tu carro esta listo para correr");
+                Encendido = 1;
+                this.velocidad_actual = 0;
+            }
+            else
+            {
+                Console.WriteLine("Ya esta encendido tu carro!!!");
+            }
+        }
+
+        public string acelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 20;
+            if (velocidad_actual <= VMaxima)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = VMaxima;
+                mensaje = $"{velocidad_actual}KPH Aguas rapido y furioso, vas muy rapido y ya llegaste al tope";
+            }
+
+          
+            Console.WriteLine(mensaje);
+            return mensaje;
+        }
+
+        public string desacelerar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"No puedes desacelerar si el carro esta apagado!!";
+                return mensaje;
+            }
+            else
+            {
+                velocidad_actual -= 5;
+                mensaje = $"Vas desacelerando, tu velocidad actual es {velocidad_actual}";
+
+            }
+            Console.WriteLine(mensaje);
+            return mensaje; 
+            
+        }
+
+        public string frenar()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = "El carro esta apagado, no podemos frenar";
+            }
+            else
+            {
+                velocidad_actual -= 10;
+                if (velocidad_actual >= 0)
+                {
+                    mensaje = $" vamos frenando, tu velocidad es {velocidad_actual}KM/H";
+
+                }
+                else
+                {
+                    mensaje = $"Ya no puedes frenar, vamos a 0KM/H";
+                }
+            }
+            
+            
+            Console.WriteLine(mensaje);
+            return mensaje; 
+        }
+
+        public void apagarmotor()
+        {
+            if (Encendido== 1)
+            {
+                Console.WriteLine("El Carro esta siendo apagado");
+                Console.WriteLine("El carro ya está apagado");
+                Encendido = 0;
+            }
+            else
+            {
+                Console.WriteLine("Lo siento! tu carro ya estaba apagado");
+            }
+        }
+
+        public string Bocinar()
+        {
+            string mensaje = "wiii wiii wiiiiii!!! tu carro esta bocinando!";
+            Console.WriteLine(mensaje); 
+            return mensaje;
+        }
+    }
+}
+       
